96d9748f93a937b8a99472933baf5b01859f3ec9
Dinorah Skidelsky <dinorahskidelsky@gmail.com>
2022-05-06 13:01:50 -0300

    rails watch list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+Movie.destroy_all
+rating_var = (0..10).to_a
+
+10.times do
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: Faker::Internet.url,
+    rating: rating_var.sample
+  )
+  movie.save
+end
